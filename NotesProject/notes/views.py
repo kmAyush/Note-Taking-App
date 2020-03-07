ceff3dd1fb5fb9e03198c5172904da35dea18c68
kmAyush <kmayush27399@gmail.com>
2020-03-08 04:38:36 +0530

    Add views and templates to show notes

--- NotesProject/notes/views.py
+++ NotesProject/notes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from .models import Note
 
-# Create your views here.
+
+def index(request):
+    noteList = Note.objects.order_by('created')
+    template = loader.get_template('notes/note.html')
+    context = {'noteList': noteList}
+    return render(request, 'notes/note.html', context)
