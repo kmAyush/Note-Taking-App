0a1de7122c6103ceb7624d136ee3eb189c8ed79b
kmAyush <kmayush27399@gmail.com>
2020-03-15 04:35:42 +0530

    Update front-end Add bootstrap,django form manually & update design

--- NotesProject/notes/models.py
+++ NotesProject/notes/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Note(models.Model): 
-	text = models.CharField(max_length = 120)
+	text = models.TextField(max_length = 120)
 	created = models.DateTimeField(auto_now = True)
